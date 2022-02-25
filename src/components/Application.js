78e4659f284a16c71e90886e04ce92553128abb9
James Phan <tjphan@Jamess-MacBook-Pro.local>
2022-02-25 15:59:26 -0800

    created helper function getInterview

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,107 +1,79 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 import "components/Application.scss";
 
 import DayList from "./DayList";
-import Appointment from "./Appointment"
+import Appointment from "./Appointment";
 
-const days = [
-  {
-    id: 1,
-    name: "Monday",
-    spots: 2,
-  },
-  {
-    id: 2,
-    name: "Tuesday",
-    spots: 5,
-  },
-  {
-    id: 3,
-    name: "Wednesday",
-    spots: 0,
-  },
-];
+import axios from "axios";
 
-
-const appointments = [
-  {
-    id: 1,
-    time: "12pm",
-  },
-  {
-    id: 2,
-    time: "1pm",
-    interview: {
-      student: "Lydia Miller-Jones",
-      interviewer:{
-        id: 3,
-        name: "Sylvia Palmer",
-        avatar: "https://i.imgur.com/LpaY82x.png",
-      }
-    }
-  },
-  {
-    id: 3,
-    time: "2pm",
-  },
-  {
-    id: 4,
-    time: "3pm",
-    interview: {
-      student: "Archie Andrews",
-      interviewer:{
-        id: 4,
-        name: "Cohana Roy",
-        avatar: "https://i.imgur.com/FK8V841.jpg",
-      }
-    }
-  },
-  {
-    id: 5,
-    time: "4pm",
-  }
-];
-
+import { getAppointmentsForDay, getInterview } from "helpers/selectors";
 
 export default function Application(props) {
-  console.log("TJ", props);
-  const [day, setDay] = useState("Monday");
-  
-  const appt = appointments.map(
-    appointment => <
-      Appointment 
-      key={appointment.id}
-      {...appointment}
-    />
-  )
+	const [state, setState] = useState({
+		day: "Monday",
+		days: [],
+		appointments: {},
+		interviewers: {},
+	});
 
-  return (
-    <main className="layout">
-      <section className="sidebar">
-        <img
-          className="sidebar--centered"
-          src="images/logo.png"
-          alt="Interview Scheduler"
-        />
-        <hr className="sidebar__separator sidebar--centered" />
-        <nav className="sidebar__menu">
-          <DayList
-            days={days}
-            value={day}
-            onChange={setDay}
-          />
-        </nav>
-        <img
-          className="sidebar__lhl sidebar--centered"
-          src="images/lhl.png"
-          alt="Lighthouse Labs"
-        />
-      </section>
-      <section className="schedule">
-        {appt}
-        <Appointment key="last" time="5pm" />
-      </section>
-    </main>
-  );
+	// generate appointments, sorted by day
+	const dailyAppointments = getAppointmentsForDay(state, state.day);
+	const appt = dailyAppointments.map((appointment) => {
+    const interview = getInterview(state, appointment.interview);
+		return <Appointment 
+    key={appointment.id} 
+    id={appointment.id}
+    time={appointment.time}
+    interview={interview}
+    />;
+	});
+
+	const setDay = (day) => setState({ ...state, day });
+
+	useEffect(() => {
+		const getDaysURL = axios.get(`/api/days`);
+		const getAppointmentsURL = axios.get(`/api/appointments`);
+		const getInterviewersURL = axios.get(`/api/interviewers`);
+
+		const promises = [getDaysURL, getAppointmentsURL, getInterviewersURL];
+		Promise.all(promises).then((all) => {
+			setState((prev) => {
+				const days = all[0].data;
+				const appointments = all[1].data;
+				const interviewers = all[2].data;
+				return {
+					...prev,
+					days,
+					appointments,
+					interviewers,
+				};
+			});
+		});
+	}, []);
+
+	return (
+		<main className='layout'>
+			<section className='sidebar'>
+				<img
+					className='sidebar--centered'
+					src='images/logo.png'
+					alt='Interview Scheduler'
+				/>
+				<hr className='sidebar__separator sidebar--centered' />
+				<nav className='sidebar__menu'>
+					<DayList days={state.days} value={state.day} onChange={setDay} />
+				</nav>
+				<img
+					className='sidebar__lhl sidebar--centered'
+					src='images/lhl.png'
+					alt='Lighthouse Labs'
+				/>
+			</section>
+			<section className='schedule'>
+				{appt}
+				<Appointment key='last' time='5pm' />
+			</section>
+		</main>
+	);
 }
---
+++ src/helpers/selectors.js
@@ -0,0 +1,24 @@
+export function getAppointmentsForDay(state, day) {
+	// match the day to the state and retrieve the array of appointments
+	const appointmentsOfMatchedDay = state.days.filter(
+		(filteredDay) => filteredDay.name === day
+	)[0];
+
+	// using the array of appointments render the appointment details
+	const matchedAppointments = [];
+	if (appointmentsOfMatchedDay) {
+		appointmentsOfMatchedDay.appointments.map((appointment) =>
+			matchedAppointments.push(state.appointments[appointment])
+		);
+	}
+	return matchedAppointments;
+}
+
+export function getInterview(state, interview) {
+	if (!interview) {
+		return null;
+	}
+  // replace the interview's id tag with the full description of the interviewer, found in state. 
+	interview.interviewer = state.interviewers[interview.interviewer];
+	return interview;
+}
